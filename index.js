5360a14b4765ddac825eadc89d65883fcf8242df
BrandonPessman <pessman.brandon@gmail.com>
2019-11-03 16:45:33 -0600

    Init files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 8001
+
+app.post('/deploy/personalwebsite', (req, res) => {
+  console.log('RECIEVED DATA FROM GITHUB')
+})
+
+app.listen(port, () => {
+  console.log('Autodeploy Server is up and running on port: ' + port)
+})
