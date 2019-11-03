fd98481a227e10537ba596b40ba0f62344dd42bb
BrandonPessman <pessman.brandon@gmail.com>
2019-11-03 16:49:42 -0600

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8001
 
-app.post('/deploy/personalwebsite', (req, res) => {
+app.post('/deploy', (req, res) => {
   console.log('RECIEVED DATA FROM GITHUB')
 })
 
