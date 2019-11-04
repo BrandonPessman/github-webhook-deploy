5cbb9dcd64f1b959062fe9337abc27dc85bc7939
brandonpessman <pessman.brandon@gmail.com>
2019-11-04 00:39:23 +0000

    Changes

--- personalWebsiteDeploy.sh
+++ personalWebsiteDeploy.sh
@@ -1,3 +1,9 @@
-echo 'deploying personal website...'
+echo 'Starting Deployment!'
 cd /var/www/personalWebsite/frontend/
 git pull
+echo 'Pulled from Github'
+npm install
+echo 'Installed Files'
+npm run build
+echo 'Build has been built'
+
