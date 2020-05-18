3e39ea22471f2c4c1e8bb9199ec665e229fbcbf0
Brandon Pessman <PESSMABM5831@uwec.edu>
2020-05-18 16:21:09 -0700

    Adds backend deployment

---
+++ fableverseBackendDeploy.sh
@@ -0,0 +1,9 @@
+echo 'Starting Deployment!'
+cd /var/www/fableverse-backend/
+git pull
+echo 'Pulled from Github'
+npm install
+echo 'Installed Files'
+pm2 restart 1
+echo 'Build has been built'
+
--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const port = 8003
 
 var execFile = require('child_process').execFile
 
-app.post('/deploy', (req, res) => {
+app.post('/deploy/frontend', (req, res) => {
   console.log('RECIEVED DATA FROM GITHUB')
   execFile('./fableverseFrontendDeploy.sh', function (error, stdout, stderr) {
+    console.log('exec complete', error, stdout, stderr)
+    res.status(200)
+  })
+
+  res.status(200)
+})
+
+app.post('/deploy/backend', (req, res) => {
+  console.log('RECIEVED DATA FROM GITHUB')
+  execFile('./fableverseBackendDeploy.sh', function (error, stdout, stderr) {
     console.log('exec complete', error, stdout, stderr)
     res.status(200)
   })
